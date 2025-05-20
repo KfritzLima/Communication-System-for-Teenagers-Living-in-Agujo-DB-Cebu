34e58a578bdc4521b587a2cdfd4f59b60a18c835
Kfritz <kfritzlima@gmail.com>
2025-05-21 06:48:55 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule],
-  template: `<router-outlet></router-outlet>`,
+  template: `
+    <div *ngIf="!loggedIn">
+      <h2>Login</h2>
+      <button (click)="login()">Log In</button>
+    </div>
+
+    <div *ngIf="loggedIn">
+      <h2>Dashboard</h2>
+      <p>Welcome, user!</p>
+      <button (click)="logout()">Log Out</button>
+    </div>
+  `,
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  loggedIn = false;
 
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
