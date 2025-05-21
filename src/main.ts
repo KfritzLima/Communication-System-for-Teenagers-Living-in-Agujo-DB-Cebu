fd898593df03fa3e02359e43bea55b285fb8009e
Kfritz <kfritzlima@gmail.com>
2025-05-21 11:01:20 +0800

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { provideRouter } from '@angular/router';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
-});
+  providers: [provideRouter(routes)],
+}).catch(err => console.error(err));
