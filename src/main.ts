69b2e181548cc47851ef9f232042fe161b403d46
Kfritz <kfritzlima@gmail.com>
2025-05-21 19:00:09 +0800

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
 }).catch(err => console.error(err));
